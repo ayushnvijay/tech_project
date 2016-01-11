f160c649ae138f92ab38f9629c13d664f58c7d25
Ayush <ayushnvijay@gmail.com>
2016-01-10 20:49:51 -0500

    First Commit

---
+++ app/src/main/java/com/ayushnvijay/android/technicalproject99co/LocationPicker.java
@@ -0,0 +1,13 @@
+package com.ayushnvijay.android.technicalproject99co;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocationPicker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_picker);
+    }
+}
